5aaa2a0bdf3882bac10c8821e81c09026674e93f
Sumit Kumar <sk4390789@gmail.com>
2023-10-27 16:29:12 +0530

    Postgres connection, table creation - @Entity, @Table, @SequenceGenerator, @Id, @GeneratedValue

--- src/main/java/com/springlearn/student/Student.java
+++ src/main/java/com/springlearn/student/Student.java
@@ -2,7 +2,19 @@ package com.springlearn.student;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     private Long id;
     private String name;
     private String email;
