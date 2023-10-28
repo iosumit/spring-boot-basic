1f403e0b6ef37df6c4442f95dba404a0120cb530
Sumit Kumar <sk4390789@gmail.com>
2023-10-28 07:40:34 +0530

    @Query, Errrors

--- src/main/java/com/springlearn/student/StudentService.java
+++ src/main/java/com/springlearn/student/StudentService.java
@@ -1,6 +1,7 @@
 package com.springlearn.student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,12 @@ public class StudentService {
 
     public void addNewStudent(Student student) {
         System.out.println(student.toString());
+        Optional<Student> studentEmail = studentRepository
+                .findStudentByEmail(student.getEmail());
+
+        if (studentEmail.isPresent()) {
+            throw new IllegalStateException("Email taken");
+        }
         studentRepository.save(student);
     }
 }
